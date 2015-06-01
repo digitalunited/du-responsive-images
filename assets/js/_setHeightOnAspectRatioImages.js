1398519c4db3471d9f83ce7fbb504da9fb35b324
Tim Samuelsson <tim@careofhaus.se>
2015-06-01 18:36:55 +0200

    Vetical alignment mande by js shuld only be done in firefox.

--- assets/js/_setHeightOnAspectRatioImages.js
+++ assets/js/_setHeightOnAspectRatioImages.js
@@ -1,55 +1,60 @@
-function defer(method) {
-    if (window.jQuery) {
-        method();
-    }
-    else {
-        setTimeout(function () {
-            defer(method)
-        }, 50);
-    }
-}
+var isFirefox = typeof InstallTrigger !== 'undefined';   // Firefox 1.0+
 
-function debounce(func, wait, immediate) {
-    var timeout;
-    return function () {
-        var context = this, args = arguments;
-        clearTimeout(timeout);
-        timeout = setTimeout(function () {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        }, wait);
-        if (immediate && !timeout) func.apply(context, args);
-    };
-}
+if (isFirefox) {
 
-var setRespImgOffset = function () {
-    $('.du-resp-img-ratio-wrapper').each(function (index, elem) {
-        $wrapper = $(elem);
-        if ($wrapper.hasClass('has-ratio')) {
-            wrapperHeight = parseInt($wrapper.css('padding-top'), 10);
-            $img = $wrapper.find('.du-resp-img').first();
-            imgHeight = $img.height();
-
-            if (imgHeight > wrapperHeight) {
-                offset = (wrapperHeight - imgHeight) / 2;
-            }
-            else {
-                offset = (imgHeight - wrapperHeight ) / 2;
-            }
-
-            $img.css({'top': offset});
+    function defer(method) {
+        if (window.jQuery) {
+            method();
         }
-    });
-};
+        else {
+            setTimeout(function () {
+                defer(method)
+            }, 50);
+        }
+    }
 
-initResponsiveImages = function () {
-    setRespImgOffset();
+    function debounce(func, wait, immediate) {
+        var timeout;
+        return function () {
+            var context = this, args = arguments;
+            clearTimeout(timeout);
+            timeout = setTimeout(function () {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            }, wait);
+            if (immediate && !timeout) func.apply(context, args);
+        };
+    }
 
-    document.addEventListener('lazybeforeunveil', function (e) {
+    var setRespImgOffset = function () {
+        $('.du-resp-img-ratio-wrapper').each(function (index, elem) {
+            $wrapper = $(elem);
+            if ($wrapper.hasClass('has-ratio')) {
+                wrapperHeight = parseInt($wrapper.css('padding-top'), 10);
+                $img = $wrapper.find('.du-resp-img').first();
+                imgHeight = $img.height();
+
+                if (imgHeight > wrapperHeight) {
+                    offset = (wrapperHeight - imgHeight) / 2;
+                }
+                else {
+                    offset = (imgHeight - wrapperHeight ) / 2;
+                }
+
+                $img.css({'top': offset});
+            }
+        });
+    };
+
+    initResponsiveImages = function () {
         setRespImgOffset();
-    });
 
-    window.addEventListener('resize', debounce(setRespImgOffset, 50));
-};
+        document.addEventListener('lazybeforeunveil', function (e) {
+            setRespImgOffset();
+        });
 
-defer(initResponsiveImages);
+        window.addEventListener('resize', debounce(setRespImgOffset, 50));
+    };
+
+    defer(initResponsiveImages);
+}
